3495091520131b49551a046099aa5f35a5540f62
Albina Dzorubova <albina.dzorubova@saltpepper.ru>
2025-12-07 13:19:10 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [message, setMessage] = useState("Привет! Это моя страница на React");
+
+  const handleClick = () => {
+    setMessage("Ты нажала кнопку! 🎉");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <h1>{message}</h1>
+      <button onClick={handleClick}>Нажми меня</button>
+      <blockquote cite="https://example.com/article">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Жизнь — это то, что с тобой происходит, пока ты строишь другие планы.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <footer>
+          — <cite>Джон Леннон</cite>
+        </footer>
+      </blockquote>
+
+      <p>Это абзац в React.</p>
+
+      <pre>Это абзац в React.</pre>
+
+      <span>Это абзац в React.</span>
+
+      <a href="https://example.com">Это ссылка в React.</a>
+
+      <b>Это жирный текст в React.</b>
+
+      <big>Это большой текст в React.</big>
+
+      <em>Это курсивный текст в React.</em>
+
+      <i>Это курсивный текст в React.</i>
+
+      <strong>Это жирный текст в React.</strong>
+
+      <sub>Это подстрочный текст в React.</sub>
+
+      <sup>Это надстрочный текст в React.</sup>
+
+      <small>Это маленький текст в React.</small>
+
+      <del>Это удаленный текст в React.</del>
+
+      <ins>Это вставленный текст в React.</ins>
+
+      <dl>
+        <dt>Это определение в React.</dt>
+        <dd>Это определение в React.</dd>
+      </dl>
+
+      <dl>
+        <dt>Это определение в React.</dt>
+        <dd>Это определение в React.</dd>
+      </dl>
     </div>
   );
 }
